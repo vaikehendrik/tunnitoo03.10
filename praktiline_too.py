896687b508263b0d67f00be5991e3e6829530f79
vaikehendrik <hendrik.paide@perg.ee>
2024-10-03 09:03:53 +0300

    Initial commit

---
+++ praktiline_too.py
@@ -0,0 +1,11 @@
+"""Iseseisev praktiline töö"""
+
+# Kirjuta kood mis:
+# 1) küsib kasutajalt ainult täisarve a ja b
+# 2) tagastab kasutajale a ja b summa, korrutise ja ruutude summa
+# all on sulle vastuse näide koos andmetüübiga f-sõne, proovi jutumärkide sisse panna loogsulud {}
+
+
+print(f"SUMMA:")
+print(f"KORRUTIS:")
+print(f"RUUTUDE SUMMA:")
---
+++ tunnitoo.py
@@ -0,0 +1,39 @@
+""""Tunnitöö 03.10"""
+
+# ül.1
+# konsooli printimine prindi konsooli tervitus
+
+
+# ül.2
+# muuda oma kood nii et see küsiks kasutajalt nime ja tervitaks teda
+# vihje: sisend saab kutsuda funktsiooniga input()
+# kui on vaja esitada konkreetne küsimus
+# siis selle saab sõne andmetüübina kirjutada funtsiooni sisse
+
+# ül.2.1
+# muuda oma kood nii et kasutaja ei peaks mõtlema liigsetele tühikutele ja suurele algustähele
+
+
+# ül.3.1
+# kontrolli oma teadmisi. Mida väljastavad kosooli kolm allolevat koodi
+# vastus kirjuta noole taha, seejärel kustuta rea alguses olev# ja kontorlli enast
+# print(type(7))  # ->
+# print(type("siin on mingisugune lause"))  # ->
+# print(type(7.0))  # ->
+
+
+# ül.3.2
+# kirjuta sulgudesse vastav pythoni termin
+# sõne (), täisarv(), ratsionaalarv()
+
+# MUUTUJAD - kuidas hoida andmeid mälus
+# matemaatikast teame muutjuaid x ja y, kuid samuti teame et tegelikult võivad nad
+# määrata kas aastaid, kogust, protsenti jne. ehk tingimisi on kokkulepe,
+# et matemaatikas kasutame ühe tähega muutujaid...
+# programmerimises võime neid nimetada nii nagu ise soovime
+# selle jaoks määrame muutjuale nime ning seejärel anname talle = märgi abil väärtuse
+# ül.4
+# mõtle välja loogiline nimetus muutujale, mis määrab sinu vanust, pane see kirja
+# Python 3.0 oli väljastatud aastal 2008
+release_year = 2008
+# mõtle välja loogiline nimetus muutujale, mis määrab pyhtoni vanust matemaatilise tehtena, pane see kirja
