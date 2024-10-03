663a0bb51f77a8af586444e1aacf7751395a4059
vaikehendrik <hendrik.paide@perg.ee>
2024-10-03 10:55:43 +0300

    Add files via upload

--- praktiline_too.py
+++ praktiline_too.py
@@ -5,7 +5,9 @@
 # 2) tagastab kasutajale a ja b summa, korrutise ja ruutude summa
 # all on sulle vastuse näide koos andmetüübiga f-sõne, proovi jutumärkide sisse panna loogsulud {}
 
+a = input("Sisesta oma a:")
+b = input("Sisesta oma b:")
 
-print(f"SUMMA:")
-print(f"KORRUTIS:")
-print(f"RUUTUDE SUMMA:")
+print(f"SUMMA:{a + b}")
+print(f"KORRUTIS: {a * b}")
+print(f"RUUTUDE SUMMA:{(a * a) + (b * b)}")
